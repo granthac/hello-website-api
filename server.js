809b48d77df514599dd1f0cff33a02e134add4f7
Levi Cook <levicook@gmail.com>
2015-11-30 16:32:14 -0700

    refactor, rename -- reinforce we're setting up an api

--- server.js
+++ server.js
@@ -1,28 +1,28 @@
 var express = require('express');
-var app = express();
+var api = express();
 
 
 // Define "create" endpoint for beta-signups:
-app.post('/api/beta-signups', function (req, res) {
+api.post('/api/beta-signups', function (req, res) {
   res.status(501).send('Not Implemented');
 });
 
 
 // Define "create" endpoint for friend-referrals
-app.post('/api/friend-referrals', function (req, res) {
+api.post('/api/friend-referrals', function (req, res) {
   res.status(501).send('Not Implemented');
 });
 
 
 // Define "create" endpoint for sales demo requests:
-app.post('/api/sales-demo-requests', function (req, res) {
+api.post('/api/sales-demo-requests', function (req, res) {
   res.status(501).send('Not Implemented');
 });
 
 
-var server = app.listen(process.env.PORT || 3000, function () {
+var server = api.listen(process.env.PORT || 3000, function () {
   var host = server.address().address;
   var port = server.address().port;
 
-  console.log('Example app listening at http://%s:%s', host, port);
+  console.log('website-api listening at http://%s:%s', host, port);
 });
